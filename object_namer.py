0b60bf517d723388bc8520c814d82acbf06fa2c9
TheKenetics <TheKenetics@users.noreply.github.com>
2019-05-23 18:04:20 -0500

    Add new operators, change panel location
    Add multi object prefix and suffix.
    Change panel location from View tab to Item tab

--- object_namer.py
+++ object_namer.py
@@ -37,11 +37,57 @@ class N_OT_multi_object_rename(bpy.types.Operator):
 		return {'FINISHED'}
 
 
+class N_OT_multi_object_add_prefix(bpy.types.Operator):
+	"""Renames multiple objects with a prefix"""
+	bl_idname = "object.n_ot_multi_object_add_prefix"
+	bl_label = "Multi Object Add Prefix"
+	bl_options = {'REGISTER','UNDO'}
+
+	# Properties
+	prefix : StringProperty(
+		name="Prefix",
+		description="Prefix to use for objects"
+	)
+
+	@classmethod
+	def poll(cls, context):
+		return context.active_object is not None
+
+	def execute(self, context):
+		for obj in context.selected_objects:
+			obj.name = self.prefix + obj.name
+		
+		return {'FINISHED'}
+
+
+class N_OT_multi_object_add_suffix(bpy.types.Operator):
+	"""Renames multiple objects with a suffix"""
+	bl_idname = "object.n_ot_multi_object_add_suffix"
+	bl_label = "Multi Object Add Suffix"
+	bl_options = {'REGISTER','UNDO'}
+
+	# Properties
+	suffix : StringProperty(
+		name="Suffix",
+		description="Suffix to use for objects"
+	)
+
+	@classmethod
+	def poll(cls, context):
+		return context.active_object is not None
+
+	def execute(self, context):
+		for obj in context.selected_objects:
+			obj.name = obj.name + self.suffix
+		
+		return {'FINISHED'}
+
+
 # Credit: Blender 2.79b
 class N_PT_view3d_name(bpy.types.Panel):
 	bl_space_type = 'VIEW_3D'
 	bl_region_type = 'UI'
-	bl_category = "View"
+	bl_category = "Item"
 	bl_label = "Item"
 
 	@classmethod
@@ -63,13 +109,17 @@ class N_PT_view3d_name(bpy.types.Panel):
 				row.label(text="", icon='BONE_DATA')
 				row.prop(bone, "name", text="")
 		
+		row = layout.row()
+		row.prop(context.window_manager.n_runtime_settings , 'multi_name', text='')
+		
 		if len(context.selected_objects) > 1:
 			row = layout.row()
-			row.label(text="Multi Object Rename")
-			row = layout.row()
-			row.prop(context.window_manager.n_runtime_settings , 'multi_name', text='')
-			row = layout.row()
 			row.operator(N_OT_multi_object_rename.bl_idname).name = context.window_manager.n_runtime_settings.multi_name
+		
+		row = layout.row()
+		row.operator(N_OT_multi_object_add_prefix.bl_idname).prefix = context.window_manager.n_runtime_settings.multi_name
+		row = layout.row()
+		row.operator(N_OT_multi_object_add_suffix.bl_idname).suffix = context.window_manager.n_runtime_settings.multi_name
 
 
 class N_runtime_settings(bpy.types.PropertyGroup):
@@ -87,11 +137,15 @@ def register():
 	)
 	
 	bpy.utils.register_class(N_OT_multi_object_rename)
+	bpy.utils.register_class(N_OT_multi_object_add_prefix)
+	bpy.utils.register_class(N_OT_multi_object_add_suffix)
 	bpy.utils.register_class(N_PT_view3d_name)
 
 
 def unregister():
 	bpy.utils.unregister_class(N_PT_view3d_name)
+	bpy.utils.unregister_class(N_OT_multi_object_add_suffix)
+	bpy.utils.unregister_class(N_OT_multi_object_add_prefix)
 	bpy.utils.unregister_class(N_OT_multi_object_rename)
 	
 	del bpy.types.WindowManager.n_runtime_settings
